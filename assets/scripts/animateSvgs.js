9f5799dc9a86ba55c9ef20f6e4918670f3e69c7d
CMietschnig <121685169+CMietschnig@users.noreply.github.com>
2024-04-29 20:57:05 +0200

    clean up and delete unneded code

--- assets/scripts/animateSvgs.js
+++ assets/scripts/animateSvgs.js
@@ -1,66 +1,64 @@
 let oneTimeCalled = false;
 
-document.addEventListener('scroll', () => showSvg("slider-1", ".svg-1"));
-document.addEventListener('scroll', () => showSvg("image-2", ".svg-2"));
+document.addEventListener("scroll", () => showSvg("slider-1", ".svg-1"));
+document.addEventListener("scroll", () => showSvg("image-2", ".svg-2"));
 
 function showSvg(imageId, pathSelector) {
-    if(isSvgOverMiddle(imageId) && !isSvgTooHigh(imageId)) {
-        if(!oneTimeCalled) {
-            drawSvg(pathSelector);
-            oneTimeCalled = true;
-        } 
+  if (isSvgOverMiddle(imageId) && !isSvgTooHigh(imageId)) {
+    if (!oneTimeCalled) {
+      drawSvg(pathSelector);
+      oneTimeCalled = true;
     }
-    else {
-        removeSvg(pathSelector);
-        oneTimeCalled = false;
-    }
+  } else {
+    removeSvg(pathSelector);
+    oneTimeCalled = false;
+  }
 }
 
-
 function isSvgOverMiddle(imageId) {
-    const projectImage = document.getElementById(imageId);
-    const projectImageRect = projectImage.getBoundingClientRect();
+  const projectImage = document.getElementById(imageId);
+  const projectImageRect = projectImage.getBoundingClientRect();
 
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
 
-    // Calculate the middle of the window height
-    const windowMiddleY = windowHeight / 2;
+  // Calculate the middle of the window height
+  const windowMiddleY = windowHeight / 2;
 
-    // Check if the top of the image is above the middle of the window height
-    return projectImageRect.top <= windowMiddleY;
+  // Check if the top of the image is above the middle of the window height
+  return projectImageRect.top <= windowMiddleY;
 }
 
 function isSvgTooHigh(imageId) {
-    const projectImage = document.getElementById(imageId);
-    const projectImageRect = projectImage.getBoundingClientRect();
+  const projectImage = document.getElementById(imageId);
+  const projectImageRect = projectImage.getBoundingClientRect();
 
-    // Check if the image is on top or above the window
-    return projectImageRect.top <= 0;
+  // Check if the image is on top or above the window
+  return projectImageRect.top <= 0;
 }
 
 function drawSvg(pathSelector) {
-    const firstProjectPaths = document.querySelectorAll(pathSelector);
+  const firstProjectPaths = document.querySelectorAll(pathSelector);
 
-    // Add a class to the paths to trigger the transition
-    firstProjectPaths.forEach((path) => {
-        path.classList.add('fill-transition');
-    });
+  // Add a class to the paths to trigger the transition
+  firstProjectPaths.forEach((path) => {
+    path.classList.add("fill-transition");
+  });
 
-    // Trigger a reflow to ensure the transition class is applied before changing the fill
-    void firstProjectPaths[0].offsetWidth;
+  // Trigger a reflow to ensure the transition class is applied before changing the fill
+  void firstProjectPaths[0].offsetWidth;
 
-    // Change the fill color to start the transition
-    firstProjectPaths.forEach((path) => {
-        path.style.strokeDashoffset = 0;
-    });
+  // Change the fill color to start the transition
+  firstProjectPaths.forEach((path) => {
+    path.style.strokeDashoffset = 0;
+  });
 }
 
 function removeSvg(pathSelector) {
-    const firstProjectPaths = document.querySelectorAll(pathSelector);
+  const firstProjectPaths = document.querySelectorAll(pathSelector);
 
-    // Remove a class to the paths to trigger the transition
-    firstProjectPaths.forEach((path) => {
-        path.classList.remove('fill-transition');
-    });
-
+  // Remove a class to the paths to trigger the transition
+  firstProjectPaths.forEach((path) => {
+    path.classList.remove("fill-transition");
+  });
 }
